5cf602617454baab6b1ceac10283fff887ccae44
Yaroslav <RaKvarz@gmail.com>
2025-12-06 22:05:08 +0200

    Lab 3.

---
+++ C#/Lab3/Task2/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NetworkSimulation
+{
+    public interface IConnectable
+    {
+        void Connect(Computer target);
+        void Disconnect(Computer target);
+        void SendData(Computer target, string data);
+        void ReceiveData(Computer source, string data);
+    }
+    public abstract class Computer : IConnectable
+    {
+        public string IP { get; set; }
+        public int Power { get; set; }
+        public string OS { get; set; }
+
+        protected List<Computer> connections = new List<Computer>();
+
+        public Computer(string ip, int power, string os)
+        {
+            IP = ip;
+            Power = power;
+            OS = os;
+        }
+
+        public virtual void Connect(Computer target)
+        {
+            if (!connections.Contains(target))
+            {
+                connections.Add(target);
+                Console.WriteLine($"{IP} під'єднано до {target.IP}");
+            }
+        }
+
+        public virtual void Disconnect(Computer target)
+        {
+            if (connections.Contains(target))
+            {
+                connections.Remove(target);
+                Console.WriteLine($"{IP} відключено від {target.IP}");
+            }
+        }
+
+        public virtual void SendData(Computer target, string data)
+        {
+            if (connections.Contains(target))
+            {
+                Console.WriteLine($"{IP} → {target.IP}: передано дані: '{data}'");
+                target.ReceiveData(this, data);
+            }
+            else
+            {
+                Console.WriteLine($"{IP} не має з'єднання з {target.IP}, дані не передано");
+            }
+        }
+
+        public virtual void ReceiveData(Computer source, string data)
+        {
+            Console.WriteLine($"{IP} отримав дані від {source.IP}: '{data}'");
+        }
+    }
+    public class Server : Computer
+    {
+        public int MaxClients { get; set; }
+
+        public Server(string ip, int power, string os, int maxClients)
+            : base(ip, power, os)
+        {
+            MaxClients = maxClients;
+        }
+    }
+
+    public class Workstation : Computer
+    {
+        public string UserName { get; set; }
+
+        public Workstation(string ip, int power, string os, string user)
+            : base(ip, power, os)
+        {
+            UserName = user;
+        }
+    }
+    public class Router : Computer
+    {
+        public int PortCount { get; set; }
+
+        public Router(string ip, int power, string os, int ports)
+            : base(ip, power, os)
+        {
+            PortCount = ports;
+        }
+
+        public override void SendData(Computer target, string data)
+        {
+            Console.WriteLine($"Маршрутизатор {IP} перенаправляє дані");
+            base.SendData(target, data);
+        }
+    }
+    public class Network
+    {
+        public List<Computer> Devices { get; set; } = new List<Computer>();
+
+        public void AddDevice(Computer comp)
+        {
+            Devices.Add(comp);
+            Console.WriteLine($"Пристрій {comp.IP} додано до мережі");
+        }
+
+        public void ConnectDevices(Computer a, Computer b)
+        {
+            a.Connect(b);
+            b.Connect(a);
+        }
+
+        public void DisconnectDevices(Computer a, Computer b)
+        {
+            a.Disconnect(b);
+            b.Disconnect(a);
+        }
+
+        public void TransferData(Computer a, Computer b, string data)
+        {
+            a.SendData(b, data);
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Network net = new Network();
+
+            var server = new Server("192.168.0.1", 900, "Linux", 100);
+            var ws = new Workstation("192.168.0.10", 300, "Windows", "User1");
+            var router = new Router("192.168.0.254", 200, "RouterOS", 4);
+
+            net.AddDevice(server);
+            net.AddDevice(ws);
+            net.AddDevice(router);
+
+            net.ConnectDevices(ws, router);
+            net.ConnectDevices(router, server);
+
+            net.TransferData(ws, server, "Hello server!");
+            net.TransferData(server, ws, "Response received.");
+
+            net.DisconnectDevices(ws, router);
+        }
+    }
+}
---
+++ C#/Lab3/Task3/Program.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CityTrafficSimulation
+{
+    public interface IDriveable
+    {
+        void Move(Road road);
+        void Stop();
+    }
+    public class Road
+    {
+        public string Name { get; set; }
+        public double Length { get; set; }
+        public double Width { get; set; } 
+        public int Lanes { get; set; }
+        public double TrafficLevel { get; set; }
+
+        public Road(string name, double length, double width, int lanes)
+        {
+            Name = name;
+            Length = length;
+            Width = width;
+            Lanes = lanes;
+            TrafficLevel = 0.0;
+        }
+
+        public void UpdateTraffic(double value)
+        {
+            TrafficLevel = Math.Clamp(value, 0, 1);
+        }
+    }
+    public abstract class Vehicle : IDriveable
+    {
+        public string Type { get; set; }
+        public double Speed { get; set; }
+        public double Size { get; set; }
+        public bool IsMoving { get; private set; }
+
+        protected Vehicle(string type, double speed, double size)
+        {
+            Type = type;
+            Speed = speed;
+            Size = size;
+        }
+
+        public virtual void Move(Road road)
+        {
+            double effectiveSpeed = Speed * (1 - road.TrafficLevel);
+            IsMoving = true;
+            Console.WriteLine($"{Type} рухається по дорозі {road.Name} зі швидкістю {effectiveSpeed:F1} км/год");
+        }
+
+        public virtual void Stop()
+        {
+            IsMoving = false;
+            Console.WriteLine($"{Type} зупинився");
+        }
+    }
+    public class Car : Vehicle
+    {
+        public Car(double speed) : base("Автомобіль", speed, 1.0) { }
+    }
+
+    public class Truck : Vehicle
+    {
+        public double CargoWeight { get; set; }
+        public Truck(double speed, double cargo) : base("Вантажівка", speed, 2.5)
+        {
+            CargoWeight = cargo;
+        }
+    }
+
+    public class Bus : Vehicle
+    {
+        public int Passengers { get; set; }
+        public Bus(double speed, int passengers) : base("Автобус", speed, 3.0)
+        {
+            Passengers = passengers;
+        }
+    }
+    public class TrafficSimulator
+    {
+        public List<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
+        public List<Road> Roads { get; set; } = new List<Road>();
+        public void AddVehicle(Vehicle v) => Vehicles.Add(v);
+        public void AddRoad(Road r) => Roads.Add(r);
+        public void SimulateStep()
+        {
+            Console.WriteLine("----- НОВИЙ КРОК СИМУЛЯЦІЇ -----");
+
+            Random rnd = new Random();
+
+            foreach (var road in Roads)
+            {
+                road.UpdateTraffic(rnd.NextDouble());
+            }
+
+            foreach (var v in Vehicles)
+            {
+                var road = Roads[rnd.Next(Roads.Count)];
+                v.Move(road);
+            }
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            var sim = new TrafficSimulator();
+
+            var road1 = new Road("Центральна", 3.5, 12, 3);
+            var road2 = new Road("Окружна", 12.0, 14, 4);
+
+            sim.AddRoad(road1);
+            sim.AddRoad(road2);
+
+            sim.AddVehicle(new Car(70));
+            sim.AddVehicle(new Truck(50, 5.0));
+            sim.AddVehicle(new Bus(40, 30));
+
+            for (int i = 0; i < 25; i++)
+            {
+                sim.SimulateStep();
+            }
+        }
+    }
+}
